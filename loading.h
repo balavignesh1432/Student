14cc6086b35a2af6e9cf0e15ad02f68450ddd6bb
balavignesh1432 <61788258+balavignesh1432@users.noreply.github.com>
2023-10-16 18:34:32 +0530

    Remove using namepace

--- loading.h
+++ loading.h
@@ -1,15 +1,13 @@
 #include<iostream>
 #include<windows.h>
 
-using namespace std;
-
 void loadingBar()
 {
 	system("cls");
 	system("color 9F");
 	
-	char a = 177, b = 219;
-	//cout<<"\n\n\n\n\n\t\t\t\t\tStudent's Data Management System";
+	char a = 177;
+	char b = 219;
 	cout<<"\n\t\t\t\t\tProject By Ajay Singh";
 	cout<<"\n\n\n\t\t\t\t\tLoading...\n";
 	cout<<endl;
@@ -18,7 +16,6 @@ void loadingBar()
 	for(int i = 0;i<26;i++)
 	{
 		cout<<a;
-	
 		cout<<"\r";
 		cout<<"\t\t\t\t\t";
 	}
